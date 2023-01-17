ab1bbd05c4cf00071976ce6d0a17173d97b43890
Saman Qaydi (Laptop) <saman.qaydi@live.com>
2023-01-17 20:47:48 +0100

    InitCommit

---
+++ src/MergeTest.ConsoleApp/Program.cs
@@ -0,0 +1,7 @@
+﻿using MergeTest.ConsoleApp;
+
+Console.WriteLine("Hello, World!");
+
+Student.FirstFunction();
+
+Student.SecondFunction();
---
+++ src/MergeTest.ConsoleApp/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace MergeTest.ConsoleApp;
+
+public static class Student
+{
+    public static void FirstFunction()
+    {
+        Console.WriteLine("First Function");
+        Console.WriteLine("First Function");
+        Console.WriteLine("First Function");
+    }
+    public static void SecondFunction()
+    {
+        Console.WriteLine("Second Function");
+        Console.WriteLine("Second Function");
+        Console.WriteLine("Second Function");
+    }
+}
