fc2207c61c274ff8c233c6303bf6c69a3ccbe2ba
Parisa Serpoushanin <parisa.serpoushani@gmail.com>
2023-01-18 09:20:35 +0330

    Added 003 Feature

--- src/MergeTest.ConsoleApp/Student.cs
+++ src/MergeTest.ConsoleApp/Student.cs
@@ -2,16 +2,6 @@
 
 public static class Student
 {
-    public static void FirstFunction()
-    {
-        Console.WriteLine("First Function");
-        Console.WriteLine("First Function");
-        Console.WriteLine("First Function");
-
-        Console.WriteLine("Parisa First");
-        Console.WriteLine("Parisa Second");
-        Console.WriteLine("Parisa Third");
-    }
     public static void SecondFunction()
     {
         Console.WriteLine("Saman Function");
