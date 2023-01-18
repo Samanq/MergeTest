320dc177f99168f5eb72542b0ba89b7bf7778081
Parisa Serpoushanin <parisa.serpoushani@gmail.com>
2023-01-18 09:28:35 +0330

    Added - Parisa005

--- src/MergeTest.ConsoleApp/Student.cs
+++ src/MergeTest.ConsoleApp/Student.cs
@@ -2,6 +2,11 @@
 
 public static class Student
 {
+    public static void FirstFunction()
+    {
+        System.Console.WriteLine("This is parisa First Function");
+        System.Console.WriteLine("End ----");
+    }
     public static void SecondFunction()
     {
         Console.WriteLine("Saman Function");
