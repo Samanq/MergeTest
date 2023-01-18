47fed9e51cef4b4622ba37e01c95633f0946f694
Saman Qaydi (Laptop) <saman.qaydi@gmail.com>
2023-01-18 06:37:07 +0100

    Added - saman feature

--- src/MergeTest.ConsoleApp/Student.cs
+++ src/MergeTest.ConsoleApp/Student.cs
@@ -10,6 +10,8 @@ public static class Student
     }
     public static void SecondFunction()
     {
+        Console.WriteLine("Saman Function");
+
         Console.WriteLine("Second Function");
         Console.WriteLine("Second Function");
         Console.WriteLine("Second Function");
